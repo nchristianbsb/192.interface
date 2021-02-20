5e2ee59f4f5049a0d6b221b48b41fcf440410a87
Nelson Silva <nelson.christian@gmail.com>
2021-02-19 21:17:18 -0500

    Projeto atualizado

--- src/model/entities/Contract.java
+++ src/model/entities/Contract.java
@@ -51,4 +51,5 @@ public class Contract {
 		return installments;
 	}
 	
+
 }
--- src/services/ContractService.java
+++ src/services/ContractService.java
@@ -7,21 +7,18 @@ import model.entities.Contract;
 import model.entities.Installment;
 
 public class ContractService {
-	
-	private OnlinePaymentService onlinePaymentService;
 
+	private OnlinePaymentService onlinePaymentService;
+	
 	public ContractService(OnlinePaymentService onlinePaymentService) {
 		this.onlinePaymentService = onlinePaymentService;
 	}
 	
 	public void processContract(Contract contract, int months) {
-		
 		double basicQuota = contract.getTotalValue() / months;
-		
-		for (int i = 1; i<= months; i++) {
-			double updateQuota = basicQuota + onlinePaymentService.interest(basicQuota, i);
-			double fullQuota = basicQuota + onlinePaymentService.paymentFee(updateQuota);
-			
+		for (int i = 1; i <= months; i++) {
+			double updatedQuota = basicQuota + onlinePaymentService.interest(basicQuota, i);
+			double fullQuota = updatedQuota + onlinePaymentService.paymentFee(updatedQuota);
 			Date dueDate = addMonths(contract.getDate(), i);
 			contract.getInstallments().add(new Installment(dueDate, fullQuota));
 		}
@@ -30,8 +27,7 @@ public class ContractService {
 	private Date addMonths(Date date, int N) {
 		Calendar calendar = Calendar.getInstance();
 		calendar.setTime(date);
-		calendar.add(calendar.MONTH, N);
+		calendar.add(Calendar.MONTH, N);	
 		return calendar.getTime();
 	}
-
 }
